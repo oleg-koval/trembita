210d93773f07b50688627b703f3cf98ef71ae745
Andrés C. Viesca Ruiz <viestat@icloud.com>
2018-01-11 15:45:37 +0100

    Edit danger file

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,6 +1,10 @@
-const { danger, warn } = require('danger');
+const { danger, warn, message } = require('danger');
+
+//test
+message(`:tada:, this worked @ ${danger.github.pr.user.login}`)
 
 // No PR is too small to include a description of why you made a change
+
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
