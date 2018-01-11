441165e494a48b09be03eeef4afeeaf1e1bf5d16
Andrés C. Viesca Ruiz <viestat@icloud.com>
2018-01-11 17:38:04 +0100

    other: Add check for .only in tests

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,7 @@
-const { danger, warn, message } = require('danger');
+const { danger, warn, message, fail } = require('danger');
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
 
 //test
 message(`:tada:, this worked @${danger.github.pr.user.login}!`)
@@ -7,4 +10,22 @@ message(`:tada:, this worked @${danger.github.pr.user.login}!`)
 
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
+}
+
+// Check test exclusion (.only) is included
+const modifiedSpecFiles = danger.git.modified_files.filter(filePath => {
+  return filePath.match(/\/test\/.+?\.(js|jsx|ts|tsx)$/gi);
+});
+
+const testFilesIncludeExclusion = modifiedSpecFiles.reduce((acc, value) => {
+  const content = fs.readFileSync(value).toString();
+  const invalid = _.includes(content, 'it.only') || _.includes(content, 'describe.only');
+  if (invalid) {
+    acc.push(path.basename(value));
+  }
+  return acc;
+}, []);
+
+if (testFilesIncludeExclusion.length > 0) {
+  fail(`An \`only\` was left in tests (${testFilesIncludeExclusion})`);
 }
