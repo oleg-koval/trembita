41f8727ebc8707a7cea5f2215aede6175f2ca830
Oleg Koval <kvl.olg@gmail.com>
2018-01-17 16:17:48 +0100

    fix: missing file

---
+++ error/trembitaError.js
@@ -0,0 +1,28 @@
+/* istanbul ignore file */
+
+/**
+ * TrembitaError constructor
+ *
+ * @param {String} msg Error message
+ * @inherits Error https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Error
+ */
+
+class TrembitaError extends Error {
+  constructor(msg) {
+    super(...arguments); // eslint-disable-line prefer-rest-params
+    if (this.captureStackTrace) {
+      this.captureStackTrace();
+    } else {
+      this.stack = new Error().stack;
+    }
+    this.message = msg;
+    Object.defineProperty(this, 'name', { value: this.constructor.name })
+  }
+}
+
+
+/*!
+ * Module exports.
+ */
+
+module.exports = TrembitaError;
